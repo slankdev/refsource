870c9c48784126f3f75651352b487e34d3c2dba0
slank@cybozu <slank.dev@gmail.com>
2016-12-08 15:18:29 +0900

    First Commit

---
+++ freebsd/crypto/openssh/version.h
@@ -0,0 +1,10 @@
+/* $OpenBSD: version.h,v 1.70 2014/02/27 22:57:40 djm Exp $ */
+/* $FreeBSD: releng/10.1/crypto/openssh/version.h 264692 2014-04-20 12:46:18Z des $ */
+
+#define SSH_VERSION	"OpenSSH_6.6.1"
+
+#define SSH_PORTABLE	"p1"
+#define SSH_RELEASE	SSH_VERSION SSH_PORTABLE
+
+#define SSH_VERSION_FREEBSD	"FreeBSD-20140420"
+#define SSH_VERSION_HPN		"_hpn13v11"
